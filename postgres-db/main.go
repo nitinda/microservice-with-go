d5ab707c3547fdc61652277c12d0afa57cc4c254
nitinda <nitinda@gmail.com>
2020-09-29 21:20:45 +0000

    Modified Code

--- postgres-db/main.go
+++ postgres-db/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
-	"time"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -14,7 +15,16 @@ const (
 	DB_NAME     = "postgres"
 )
 
-// type DBSts struct{}
+type userInfoSummary struct {
+	ID         int
+	UserName   string
+	DepartName string
+	Created    string
+}
+
+type userinfos struct {
+	UserInfos []userInfoSummary
+}
 
 func main() {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
@@ -23,32 +33,49 @@ func main() {
 	checkErr(err)
 	defer db.Close()
 
-	fmt.Println("# Inserting values")
+	// fmt.Println("# Inserting values")
 
 	var lastInsertId int
-	err = db.QueryRow("INSERT INTO userinfo(username,departname,created) VALUES($1,$2,$3) returning uid;", "astaxie", "研发部门", "2012-12-09").Scan(&lastInsertId)
+	err = db.QueryRow("INSERT INTO userinfo(username,departname,created) VALUES($1,$2,$3) returning uid;", "astaxie", "asdasd", "2012-12-09").Scan(&lastInsertId)
 	checkErr(err)
-	fmt.Println("last inserted id =", lastInsertId)
+	// fmt.Println("last inserted id =", lastInsertId)
 
-	fmt.Println("# Querying")
+	repos := userinfos{}
+
+	// fmt.Println("# Querying")
 	rows, err := db.Query("SELECT * FROM userinfo")
 	checkErr(err)
+	defer rows.Close()
 
 	for rows.Next() {
-		var uid int
-		var username string
-		var department string
-		var created time.Time
-		err = rows.Scan(&uid, &username, &department, &created)
-		checkErr(err)
-		fmt.Println("uid | username | department | created ")
-		fmt.Printf("%3v | %8v | %6v | %6v\n", uid, username, department, created)
+		repo := userInfoSummary{}
+		err = rows.Scan(
+			&repo.ID,
+			&repo.UserName,
+			&repo.DepartName,
+			&repo.Created,
+		)
+
+		repos.UserInfos = append(repos.UserInfos, repo)
 	}
 
-	DBSt := db.Stats()
+	// for rows.Next() {
+	// 	var uid int
+	// 	var username string
+	// 	var department string
+	// 	var created time.Time
+	// 	err = rows.Scan(&uid, &username, &department, &created)
+	// 	checkErr(err)
+	// 	fmt.Println("uid | username | department | created ")
+	// 	fmt.Printf("%3v | %8v | %6v | %6v\n", uid, username, department, created)
+	// }
 
-	fmt.Println(DBSt)
+	out, err := json.Marshal(repos)
+	if err != nil {
+		fmt.Println("============Error=======")
+	}
 
+	fmt.Fprintf(os.Stdout, string(out))
 }
 
 func checkErr(err error) {
